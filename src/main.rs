39d049e32be18fea8d834facad7700d2de7110ac
rtulip <robert.tulip97@gmail.com>
2019-05-20 13:30:53 -0700

    Added piston

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 #![deny(missing_docs)]
 
 //! A roguelike tower-defense game.
-
+extern crate piston;
 extern crate rust_game;
+
 use rust_game::level::{Level,LevelController,LevelView, LevelViewSettings};
 use rust_game::misc::random;
 use rust_game::input;
+
+use piston::window::WindowSettings;
 
 fn main() {
 
@@ -17,5 +20,9 @@ fn main() {
 
     let settings = LevelViewSettings::new();
     let lvlview = LevelView::new(settings);
+
+    let window_settings = WindowSettings::new("Game", [512;2]).exit_on_esc(true);
+
+    println!("{}", window_settings.get_exit_on_esc());
 
 }
