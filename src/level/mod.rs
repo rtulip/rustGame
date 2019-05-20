321963dbbc563553cc35697ed91a50566dc30e7d
rtulip <robert.tulip97@gmail.com>
2019-05-20 13:13:53 -0700

    Added LevelView and LevelViewSettings

--- src/level/mod.rs
+++ src/level/mod.rs
@@ -1,4 +1,6 @@
 pub use self::level_model::{Level, Map};
 pub use self::level_controller::LevelController;
+pub use self::level_view_settings::LevelViewSettings;
 mod level_model;
 mod level_controller;
+mod level_view_settings;
