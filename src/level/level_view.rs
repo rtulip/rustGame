08741bab34b7935cc421ef18e1652f740246e8bf
rtulip <robert.tulip97@gmail.com>
2019-05-20 13:49:02 -0700

    merged level_view_settings.rs and level_view.rs

--- src/level/level_view.rs
+++ src/level/level_view.rs
@@ -1,4 +1,23 @@
-use crate::level::LevelViewSettings;
+pub struct LevelViewSettings {
+
+    pub graphics_position: [f64; 2],
+    pub size: f64,
+    pub tile_size: f64,
+
+}
+
+impl LevelViewSettings {
+
+    pub fn new() -> LevelViewSettings {
+
+        LevelViewSettings{
+            graphics_position: [10.0; 2],
+            size: 500.0,
+            tile_size: 10.0, 
+        }
+    }
+}
+
 
 pub struct LevelView {
 
@@ -12,4 +31,8 @@ impl LevelView {
         LevelView {settings: settings}
     }
     
+    // pub fn draw<G: Graphics>(&self, controller: &GameboardController, c: &Context, g: &mut G) {
+    //     TODO
+    // }
+
 }
--- src/level/mod.rs
+++ src/level/mod.rs
@@ -1,8 +1,6 @@
 pub use self::level_model::{Level, Map};
 pub use self::level_controller::LevelController;
-pub use self::level_view_settings::LevelViewSettings;
-pub use self::level_view::LevelView;
+pub use self::level_view::{LevelView, LevelViewSettings};
 mod level_model;
 mod level_controller;
-mod level_view_settings;
 mod level_view;
