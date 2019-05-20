cf73208f8f5bc79edd277bfac6c6b9f9081d390f
rtulip <robert.tulip97@gmail.com>
2019-05-20 15:22:28 -0700

    Added new function for Player

--- src/entity/player/player_model.rs
+++ src/entity/player/player_model.rs
@@ -1,4 +1,13 @@
+const STARTING_HEALTH
 pub struct Player{
-     position: [f64; 2],
-     health: i32,
+    pub position: [f64; 2],
+    pub health: i32,
+}
+
+impl Player {
+
+    pub fn new(start_position: [f64; 2]) -> Player {
+        Player{position: start_position, health: STARTING_HEALTH}
+    }
+
 }
