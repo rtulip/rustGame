98f49a7d3e9919bddc30e460aa84802d647bf427
rtulip <robert.tulip97@gmail.com>
2019-05-20 14:42:54 -0700

    Can draw Level

--- src/level/level_view.rs
+++ src/level/level_view.rs
@@ -1,30 +1,35 @@
 use graphics::types::Color;
 use graphics::{Context, Graphics};
 use crate::level::LevelController;
+use crate::entity::tile::Tile;
 
-pub const WINDOW_WIDTH: f64 = 1024.0;
-pub const WINDOW_HEIGHT: f64 = 812.0;
+pub const WINDOW_WIDTH: f64 = 1000.0;
+pub const WINDOW_HEIGHT: f64 = 1000.0;
 
 pub struct LevelViewSettings {
 
-    pub graphics_position: [f64; 2],
     pub tile_size: f64,
     pub background_color: Color,
+    pub error_color: Color,
+    pub floor_color: Color,
+    pub wall_color: Color,
 
 }
 
 impl LevelViewSettings {
 
-    
     pub fn new() -> LevelViewSettings {
 
         LevelViewSettings{
-            graphics_position: [0.0; 2],
-            tile_size: 10.0,
-            background_color: [0.8, 0.8, 1.0, 1.0],
-
+            tile_size: 20.0,
+            background_color: [0.0, 0.0, 0.0, 1.0],
+            error_color: [1.0, 0.0, 0.0, 1.0],
+            wall_color: [0.3, 0.3, 0.2, 1.0],
+            floor_color: [0.2, 0.13, 0.08, 1.0],
         }
     }
+
+
 }
 
 pub struct LevelView {
@@ -43,12 +48,28 @@ impl LevelView {
         use graphics::{Line, Rectangle};
 
         let settings = &self.settings;
-        let bounding_box = [settings.graphics_position[0],
-                            settings.graphics_position[1],
-                            WINDOW_WIDTH,
-                            WINDOW_HEIGHT];
-
-        Rectangle::new(settings.background_color).draw(bounding_box, &c.draw_state, c.transform, g);
+        
+        for h in 0..controller.get_height(){
+            for w in 0..controller.get_width(){
+                match controller.get_map().get(&(w,h)){
+                    Some(Tile::Floor) => {
+                        Rectangle::new(settings.floor_color)
+                            .draw([w as f64 * settings.tile_size, h as f64 * settings.tile_size, settings.tile_size,settings.tile_size], 
+                                  &c.draw_state, c.transform, g);
+                    },
+                    Some(Tile::Wall) => {
+                        Rectangle::new(settings.wall_color)
+                            .draw([w as f64 * settings.tile_size, h as f64 * settings.tile_size, settings.tile_size,settings.tile_size], 
+                                  &c.draw_state, c.transform, g);
+                    },
+                    _ => {
+                        Rectangle::new(settings.error_color)
+                            .draw([w as f64 * settings.tile_size, h as f64 * settings.tile_size, settings.tile_size,settings.tile_size], 
+                                  &c.draw_state, c.transform, g);
+                    },
+                }; 
+            }
+        }
 
     }
 
