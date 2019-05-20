e7ea0cd4ff6cd6cdb5539ea626cb310a1dda4e64
rtulip <robert.tulip97@gmail.com>
2019-05-20 13:16:32 -0700

    Added level_view to mod.rs

--- src/level/mod.rs
+++ src/level/mod.rs
@@ -1,6 +1,8 @@
 pub use self::level_model::{Level, Map};
 pub use self::level_controller::LevelController;
 pub use self::level_view_settings::LevelViewSettings;
+pub use self::level_view::LevelView;
 mod level_model;
 mod level_controller;
 mod level_view_settings;
+mod level_view;
