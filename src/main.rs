69b43aad09cef91e104e166ddafc5f29152855b7
rtulip <robert.tulip97@gmail.com>
2019-05-20 13:44:29 -0700

    Added OpenGl and window is now white

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 //! A roguelike tower-defense game.
 extern crate piston;
 extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
 extern crate rust_game;
 
 use rust_game::level::{Level,LevelController,LevelView, LevelViewSettings};
@@ -11,7 +13,9 @@ use rust_game::input;
 
 use piston::window::WindowSettings;
 use piston::event_loop::{Events, EventSettings};
+use piston::input::RenderEvent;
 use glutin_window::GlutinWindow;
+use opengl_graphics::{OpenGL, GlGraphics};
 
 fn main() {
 
@@ -24,11 +28,20 @@ fn main() {
     let settings = LevelViewSettings::new();
     let lvlview = LevelView::new(settings);
 
-    let window_settings = WindowSettings::new("Game", [512;2]).exit_on_esc(true);
+    let opengl = OpenGL::V3_2;
+
+    let window_settings = WindowSettings::new("Game", [512;2]).opengl(opengl).exit_on_esc(true);
     let mut window: GlutinWindow = window_settings.build().expect("Couldn't create window!");
     let mut events = Events::new(EventSettings::new());
+    let mut gl = GlGraphics::new(opengl);
 
     while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args(){
+            gl.draw(args.viewport(), |c, g| {
+                use graphics::{clear};
 
+                clear([1.0; 4], g);
+            });
+        }
     }
 }
