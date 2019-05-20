1cdbf73c12a88bdc8768c71e81f5c939bc20b077
rtulip <robert.tulip97@gmail.com>
2019-05-19 18:18:27 -0700

    Fill Walls (#4)
    * Identify and fill untraversable space
    
    * comments
    
    * cleanup

--- src/main.rs
+++ src/main.rs
@@ -181,6 +181,104 @@ fn create_seed(debug: bool) -> <ChaChaCore as SeedableRng>::Seed {
     }
 }
 
+/// fill_walls()
+/// 
+/// args:
+///     mut map: Map: A mutable version of the Map to be filled.
+///     width: i32: The width of the map.
+///     height: i32: The height of the map.
+/// 
+/// returns: An augmented version of the map which has only one traversable
+/// area. The connected area of the map is chosen, every other space is 
+/// converted into a Tile::Wall. 
+/// 
+/// Filling the walls follows a 3 step process:
+///     1. Traverse all tiles in the map and flood_fill() each Floor:Tile. 
+///     2. Find the largest traversable area.
+///     3. Traverse all tiles in the map again, and convert the largest 
+///        traversable area into Tile::Floor and everything else into
+///        Tile::Wall.
+fn fill_walls(mut map: Map, width: i32, height:i32) -> Map {
+
+    /// flood_fill()
+    /// 
+    /// args:
+    ///     mut map: &mut Map: The Map which is being traversed.
+    ///     start: &Pos: The starting position of the flood fill.
+    ///     new_val: &i32: The replacement value of the flood fill.
+    ///     sets: &mut HashMap<i32, i32>: A hashmap to track the sets of 
+    ///         traversable area and their sizes.
+    /// 
+    /// Convertes all reachable Tile::Floor from start to Tile::Cust(new_val)
+    fn flood_fill(mut map: &mut Map, start: &Pos, new_val: &i32, sets: &mut HashMap<i32, i32>){
+        match map.get(start){
+            Some(Tile::Floor) => {
+                map.remove(start);
+                map.insert(*start, Tile::Cust(*new_val));
+                *sets.entry(*new_val).or_insert(1) += 1;
+                flood_fill(&mut map, &(start.0, start.1-1), new_val, sets);
+                flood_fill(&mut map, &(start.0, start.1+1), new_val, sets);
+                flood_fill(&mut map, &(start.0-1, start.1), new_val, sets);
+                flood_fill(&mut map, &(start.0+1, start.1), new_val, sets);
+            },
+            _ => ()
+        }
+    }
+
+    // Used to track the number of different regions in the Map.
+    let mut region = 0;
+    // Used to count the sizes of each different region.
+    // Key: region number & Value: region count.
+    let mut sets: HashMap<i32, i32> = HashMap::new();
+    
+    // Traverse map and flood_fill each Tile::Floor.
+    for h in 0..height {
+        for w in 0..width {
+
+            match map.get(&(w,h)){
+                Some(Tile::Floor) => {
+                    flood_fill(&mut map, &(w,h), &region, &mut sets);
+                    // increment region counter.
+                    region += 1;
+                },
+                _ => ()
+            }
+
+        }
+    }
+
+    // Find the region in sets with the largest number of traversable spaces.
+    let mut max = (-1,-1);
+    for (region, count) in sets {
+        if count > max.1 {
+            max = (region, count);
+        }
+    }
+
+    // Convert back to Tile::Floor and Tile::Wall
+    for h in 0..height {
+        for w in 0..width {
+            
+            match map.get(&(w,h)) {
+                // If Tile is of the most traversable region, convert to floor
+                Some(Tile::Cust(max_val)) if *max_val == max.0 => {
+                    map.remove(&(w,h));
+                    map.insert((w,h), Tile::Floor);
+                },
+                // If Tile is of another region, convert to Wall.
+                Some(Tile::Cust(_val)) => {
+                    map.remove(&(w,h));
+                    map.insert((w,h), Tile::Wall);
+                },
+                _ => ()
+                 
+            }
+        }
+    }
+
+    map
+}
+
 /// generate_map()
 /// 
 /// args: 
@@ -217,6 +315,9 @@ fn generate_map(rng: &mut ChaChaRng, width: i32, height: i32, iters: i32) -> Map
             }
         }
     }
+    
+    // Fill untraversable space with walls
+    map = fill_walls(map, width, height);
 
     map
 }
