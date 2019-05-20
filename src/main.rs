2e4acdd8f58549e4f0ee13e42a893f52196d868c
rtulip <robert.tulip97@gmail.com>
2019-05-20 11:29:30 -0700

    Created entity module which includes tile

--- src/main.rs
+++ src/main.rs
@@ -3,43 +3,14 @@ extern crate rand_core;
 extern crate rand;
 
 extern crate rust_game;
-use rust_game::position::Pos;
+use rust_game::position::{Pos};
+use rust_game::entity::tile::{Tile};
 
-
-use std::collections::HashMap;
-use std::fmt;
 use std::env;
 
+use std::collections::HashMap;
 use rand_chacha::{ChaChaCore, ChaChaRng};
 use rand_core::{SeedableRng,RngCore};
-
-/// Tile Enum
-/// 
-/// Used to represent different types of tiles which can be found in the map.
-/// Custom tiles can be created, but must contain a i32 which can be printed. 
-/// 
-/// Tiles implement fmt::Display so that a tile can be printed.
-enum Tile {
-    Floor,
-    Wall,
-    Cust(i32),
-}
-
-/// Tile Display implementation
-/// 
-/// Floors are written as a "."
-/// Walls are written as a "W"
-/// Custome tiles are written as whatever i32 is provided  
-impl fmt::Display for Tile{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
-        match *self{
-            Tile::Floor => write!(f, "."),
-            Tile::Wall => write!(f, "W"),
-            Tile::Cust(i) => write!(f, "{}", i)
-        }
-    }
-}
-
 
 /// Map Type
 /// 
