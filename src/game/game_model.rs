d1ebe6ebc5edd98257d9641568544f4f625a5dbf
rtulip <robert.tulip97@gmail.com>
2019-05-23 14:14:52 -0700

    Game Model (#26)
    * Added GameModel Struct
    
    * updated game/mod.rs
    
    * did some rng stuff

---
+++ src/game/game_model.rs
@@ -0,0 +1,24 @@
+use crate::level::Level;
+use crate::misc::random::{Seed, RNG, from_seed, next_u32};
+use crate::traits::Model;
+
+pub struct GameModel {
+    // beacon: Beacon,
+    // entities: Vec<Entity>,
+    level: Level,
+    rng: RNG,
+}
+
+impl GameModel {
+    pub fn new(seed: Seed) -> Self {
+        let mut level = Level::new(seed);
+        let mut rng = from_seed(seed);
+        for i in 0..level.get_width() * level.get_height() + 100 {
+            next_u32(&mut rng);
+        }
+        Self {
+            level: level,
+            rng: rng,
+        }
+    }
+}
