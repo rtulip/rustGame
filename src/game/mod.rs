1af91fdb3a49687b6ca60c7a93f6db790f57423c
rtulip <robert.tulip97@gmail.com>
2019-05-23 14:32:10 -0700

    Updated mod.rs

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,6 +1,8 @@
 pub use self::game_struct::Game;
 pub use self::game_model::GameModel;
 pub use self::game_controller::GameController;
+pub use self::game_view::GameView;
 mod game_struct;
 mod game_model;
 mod game_controller;
+mod game_view;
