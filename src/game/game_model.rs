61b1f91fadd2a53edd4aa989196f0881c513eba7
rtulip <robert.tulip97@gmail.com>
2019-05-23 14:13:55 -0700

    did some rng stuff

--- src/game/game_model.rs
+++ src/game/game_model.rs
@@ -1,5 +1,5 @@
 use crate::level::Level;
-use crate::misc::random::{Seed, RNG, from_seed};
+use crate::misc::random::{Seed, RNG, from_seed, next_u32};
 use crate::traits::Model;
 
 pub struct GameModel {
@@ -13,7 +13,9 @@ impl GameModel {
     pub fn new(seed: Seed) -> Self {
         let mut level = Level::new(seed);
         let mut rng = from_seed(seed);
-        
+        for i in 0..level.get_width() * level.get_height() + 100 {
+            next_u32(&mut rng);
+        }
         Self {
             level: level,
             rng: rng,
