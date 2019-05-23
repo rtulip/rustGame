d61ab2c1b43177f00fb7e6e1980160b265481ad8
rtulip <robert.tulip97@gmail.com>
2019-05-23 14:19:54 -0700

    Added Game Controller Struct

---
+++ src/game/game_controller.rs
@@ -0,0 +1,23 @@
+use crate::game::GameModel;
+use crate::misc::random::Seed;
+
+use piston::input::GenericEvent;
+
+pub struct GameController {
+    model: GameModel;
+}
+
+impl GameController {
+    pub fn new(seed: Seed) -> Self {
+        Self {model: GameModel::new(seed) }
+    }
+
+    pub fn handle_event<E: GenericEvent>(&mut self, e: &E) {
+        // TODO
+    }
+
+    pub fn tick(&self) {
+        // TODO
+    }
+
+}
