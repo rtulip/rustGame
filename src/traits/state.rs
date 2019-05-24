acdd1b2fb017fa4017228fd2cbb7333c68eea028
rtulip <robert.tulip97@gmail.com>
2019-05-23 17:14:29 -0700

    Impl State for Player

--- src/traits/state.rs
+++ src/traits/state.rs
@@ -2,6 +2,6 @@ use crate::traits::entity::Entity;
 
 pub trait State: Entity {
     type StateEnum;
-    fn change_state(&self, new_state: Self::StateEnum);
+    fn change_state(&mut self, new_state: Self::StateEnum);
 }
 
