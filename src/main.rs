e2f918959c4313a20e05ea3aff696398afa1ebc4
rtulip <robert.tulip97@gmail.com>
2019-05-23 22:34:29 -0700

    Main cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
-#![deny(missing_docs)]
-
-//! A roguelike tower-defense game.
-extern crate piston;
-extern crate glutin_window;
-extern crate graphics;
-extern crate opengl_graphics;
-extern crate rust_game;
-
 use rust_game::game::Game;
 use rust_game::input;
 use rust_game::misc::random;
 
 fn main() {
-
+    
     let config = input::handle_init_input();
     let seed = random::create_seed(config.debug);
 
