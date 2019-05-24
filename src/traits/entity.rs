7ff9d3b75ab042db6ded0b65b4ca5a8057e0d115
rtulip <robert.tulip97@gmail.com>
2019-05-23 22:58:26 -0700

    entity.rs Documentation

--- src/traits/entity.rs
+++ src/traits/entity.rs
@@ -1,5 +1,9 @@
 use crate::traits::shape::Shape;
 
+/// Entity Trait
+/// 
+/// Shape is a supertrait of Entity
+/// tick() is a method to update the Entity once per game loop
 pub trait Entity: Shape {
 
     fn tick(&mut self);
