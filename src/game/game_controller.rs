802292b1f8eafe96bf8217df3c134d18f48a4a0e
rtulip <robert.tulip97@gmail.com>
2019-05-24 23:39:51 -0700

    initial collision prototype

--- src/game/game_controller.rs
+++ src/game/game_controller.rs
@@ -2,7 +2,8 @@ use crate::game::{GameModel, GameView};
 use crate::misc::random::Seed;
 use crate::traits::entity::Entity;
 use crate::traits::state::State;
-use crate::entity::player::PlayerState;
+use crate::entity::{player, tile};
+
 use std::collections::HashSet;
 
 use piston::input::{GenericEvent, Button, Key};
@@ -60,23 +61,75 @@ impl GameController {
         ] {
             [true, true] => {
                 self.model.player.backwards = false;
-                self.model.player.change_state(PlayerState::Stationary);
+                self.model.player.change_state(player::PlayerState::Stationary);
             },
             [true, false] => {
                 self.model.player.backwards = false;
-                self.model.player.change_state(PlayerState::Moving);
+                self.model.player.change_state(player::PlayerState::Moving);
             },
             [false, true] => {
                 self.model.player.backwards = true;
-                self.model.player.change_state(PlayerState::Moving);
+                self.model.player.change_state(player::PlayerState::Moving);
             },
             [false, false] => {
                 self.model.player.backwards = false;
-                self.model.player.change_state(PlayerState::Stationary);
+                self.model.player.change_state(player::PlayerState::Stationary);
             }
         }
         
         self.model.player.tick();
+        self.check_player_collision();
+    }
+
+    fn check_player_collision(&mut self) {
+        let tile_size = self.view.settings.tile_size;
+        let player_size = self.view.settings.player_size;
+        let player_pos = self.model.player.position;
+
+        let min_x = ( player_pos[0] / tile_size).floor() as i32;
+        let max_x = ((player_pos[0] + player_size) / tile_size).floor() as i32 + 1;
+
+        let min_y = ( player_pos[1] / tile_size).floor() as i32;
+        let max_y = ((player_pos[1] + player_size) / tile_size).floor() as i32 + 1;
+        
+        let mut found_collision = false;
+        for h in min_y..max_y {
+            for w in min_x..max_x {
+                match self.model.level.map.get(&(w,h)) {
+                    Some(tile::Tile::Wall) => {
+                        let tile_pos = [w as f64 * tile_size, h as f64 * tile_size];
+
+                        match [tile_pos[0] < player_pos[0], tile_pos[1] < player_pos[1]] {
+                            [true, true] => {
+                                self.model.player.position[0] += tile_pos[0] + tile_size - player_pos[0] + 0.1;
+                                self.model.player.position[1] += tile_pos[1] + tile_size - player_pos[1] + 0.1;
+                            },
+                            [false, true] => {
+                                self.model.player.position[0] += tile_pos[0] - player_pos[0] - player_size - 0.1;
+                                self.model.player.position[1] += tile_pos[1] + tile_size - player_pos[1] + 0.1;
+                            },
+                            [true, false] => {
+                                self.model.player.position[0] += tile_pos[0] + tile_size - player_pos[0] + 0.1;
+                                self.model.player.position[1] += tile_pos[1] - player_pos[1] - player_size - 0.1;
+                            }, 
+                            [false, false] => {
+                                self.model.player.position[0] += tile_pos[0] - player_pos[0] - player_size - 0.1;
+                                self.model.player.position[1] += tile_pos[1] - player_pos[1] - player_size - 0.1;
+                            }
+                        }
+                        
+                        found_collision = true;
+                    },
+                    _ => {}
+                }
+            }
+        }
+
+        if found_collision {
+            println!("Collision");
+        } else {
+            println!("No Collision");
+        }
     }
 
 }
