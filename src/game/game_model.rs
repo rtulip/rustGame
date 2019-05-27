c633e6c893fd089a88e39c84eedf9ba565ec9377
rtulip <robert.tulip97@gmail.com>
2019-05-27 09:52:17 -0700

    Added enemies to game_model

--- src/game/game_model.rs
+++ src/game/game_model.rs
@@ -3,15 +3,16 @@ use crate::misc::random::{Seed, RNG, from_seed, next_u32};
 use crate::entity::player::Player;
 use crate::entity::tile::Tile;
 use crate::entity::beacon::Beacon;
+use crate::entity::enemy::Enemy;
 
 /// GameModel 
 /// 
 /// A model of the games entities and controls game logic
 pub struct GameModel {
-    // entities: Vec<Entity>,
     pub level: Level,
     pub player: Player,
     pub beacon: Beacon,
+    pub enemies: Vec<Enemy>,
     rng: RNG,
 }
 
@@ -23,12 +24,15 @@ impl GameModel {
         let mut beacon = Beacon::new(beacon_spawn);
         let player_spawn = GameModel::find_player_spawn(&level, &beacon, &mut rng);
         let mut player = Player::new([player_spawn.x as f64 * 20.0, player_spawn.y as f64 * 20.0]);
-        
+        let enemy_spawn = GameModel::find_enemy_spawn(&level, &mut rng);
+        let enemy = Enemy::new([enemy_spawn.x as f64 * 20.0, enemy_spawn.y as f64 * 20.0]);
+        let mut enemies = vec![enemy];
         Self {
             level: level,
             rng: rng,
             player: player,
             beacon: beacon,
+            enemies: enemies,
         }
     }
 
@@ -122,5 +126,40 @@ impl GameModel {
         idx
 
     } 
+
+    /// find_enemy_spawn()
+    /// 
+    /// args:
+    ///     level: &Level: A reference to the level to serach for a player 
+    ///         spwan point
+    ///     rng: &mut RNG: A mutable reference to a random number generator
+    ///         which is used to decide which of the open spaces is to be the
+    ///         spawn point
+    /// 
+    /// Chooses a spawn point randomly from any Tile::Floor spaces in the Level
+    fn find_enemy_spawn(level: &Level, rng: &mut RNG) -> MapIdx {
+
+        let mut spawnable_spaces: Vec<MapIdx> = Vec::new();
+
+        for h in 0..level.height {
+            for w in 0..level.width {
+                match level.map.get(&MapIdx::new(w,h)) {
+                    Some(Tile::Floor) => {
+                        spawnable_spaces.push(MapIdx::new(w,h));
+                    },
+                    _ => (),
+                }
+            }
+        }
+
+        if spawnable_spaces.len() == 0 {
+            panic!("No spawnable spaces!");
+        }
+
+        let idx = next_u32(rng) as usize % spawnable_spaces.len();
+        let idx = spawnable_spaces.remove(idx);
+        idx
+
+    }
 
 }
