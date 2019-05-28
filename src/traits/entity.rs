6087323dea9ac180e7c7f3712e104e0cc1e63763
rtulip <robert.tulip97@gmail.com>
2019-05-28 11:38:06 -0700

    updated traits docs

--- src/traits/entity.rs
+++ src/traits/entity.rs
@@ -1,7 +1,5 @@
 use crate::traits::shape::Shape;
 
-/// Entity Trait
-/// 
 /// Shape is a supertrait of Entity
 /// tick() is a method to update the Entity once per game loop
 pub trait Entity: Shape {
--- src/traits/state.rs
+++ src/traits/state.rs
@@ -1,5 +1,3 @@
-/// State Trait
-/// 
 /// A trait to model state-based Entities. 
 /// Entity is a supertrait of State. (i.e. Implementing State requies Entity 
 /// to have been implemented)
@@ -9,12 +7,7 @@
 /// 
 /// fn change_state()
 /// 
-///     args: 
-///         &mut Self: A mutable version of the Entity. Mutability is
-///             required in order to change internal state variables
-///         new_state: Self::StateEnum: The new state the Entity is to take on
-///     
-///     Changes the state of the Entity to a new state.
+///     Defines how to changes the state of the Entity to a new state.
 pub trait State {
     type StateEnum;
     fn change_state(&mut self, new_state: Self::StateEnum);
