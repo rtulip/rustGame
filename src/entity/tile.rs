ac311cee98a1bfc81e40330230470582114b2de6
rtulip <robert.tulip97@gmail.com>
2019-05-29 16:41:01 -0700

    Reworked tile.rs - Wont Compile

--- src/entity/tile.rs
+++ src/entity/tile.rs
@@ -1,17 +1,70 @@
-use crate::traits::shape;
-
 /// Used to represent different types of tiles which can be found in the map.
-/// Custom tiles can be created, but must contain a i32 which can be printed. 
-pub enum Tile {
+use crate::traits::draw::{GenericShape,ShapeVariant};
+use crate::level::MapIdx;
+use crate::game::consts::{
+    TILE_SIZE,
+    FLOOR_COLOR,
+    WALL_COLOR,
+    SPAWNER_COLOR,
+    ERROR_COLOR,
+    map_idx_to_point2,
+};
+pub enum TileVariant {
     Floor,
     Wall,
     Spawner,
     Cust(i32),
 }
 
-impl shape::Shape for Tile {
-    type ShapeVairant = shape::RectangleType;
-    fn get_shape(&self) -> Self::ShapeVairant {
-        shape::RectangleType {}
+pub struct Tile {
+    variant: TileVariant,
+    shape: GenericShape,
+}
+
+impl Tile {
+    pub fn new(variant: TileVariant, idx: MapIdx) -> Self {
+        match variant {
+            TileVariant::Floor => {
+                Self {
+                    variant: variant,
+                    shape: GenericShape::new(
+                        ShapeVariant::Square{size: TILE_SIZE},
+                        FLOOR_COLOR, 
+                        map_idx_to_point2(idx)
+                    ),
+                }
+            },
+            TileVariant::Wall => {
+                Self {
+                    variant: variant,
+                    shape: GenericShape::new(
+                        ShapeVariant::Square{size: TILE_SIZE},
+                        WALL_COLOR, 
+                        map_idx_to_point2(idx)
+                    ),
+                }
+            },
+            TileVariant::Spawner => {
+                Self {
+                    variant: variant,
+                    shape: GenericShape::new(
+                        ShapeVariant::Square{size: TILE_SIZE},
+                        SPAWNER_COLOR, 
+                        map_idx_to_point2(idx)
+                    ),
+                }
+            },
+            TileVariant::Cust(_) => {
+                Self {
+                    variant: variant,
+                    shape: GenericShape::new(
+                        ShapeVariant::Square{size: TILE_SIZE},
+                        ERROR_COLOR, 
+                        map_idx_to_point2(idx)
+                    ),
+                }
+            }
+        }
+        
     }
 }
