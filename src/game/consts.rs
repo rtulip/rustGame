5449727af1edb3112a06f6c7ebb1b56acae08c36
rtulip <robert.tulip97@gmail.com>
2019-05-29 15:46:41 -0700

    Enemy rendering update (#88)
    * Updated Enemy to use Draw
    
    * Removed unused imports

--- src/game/consts.rs
+++ src/game/consts.rs
@@ -4,3 +4,8 @@ pub const PLAYER_RADIUS: f64 = PLAYER_SIZE/2.0;
 pub const PLAYER_STARTING_HEALTH: i32 = 10000;
 pub const PLAYER_SPEED: f64 = 0.1;
 pub const PLAYER_COLOR: Color = [0.75, 0.12, 0.08,1.0];
+
+pub const ENEMY_SIZE: f64 = 16.0;
+pub const ENEMY_RADIUS: f64 = ENEMY_SIZE/2.0;
+pub const ENEMY_COLOR: Color = [0.04, 0.13, 0.27, 1.0];
+pub const ENEMY_SPEED: f64 = 0.1;
