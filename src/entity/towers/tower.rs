4e8ccdd1ce1aa70b5edce092bfefb4473ab22812
rtulip <robert.tulip97@gmail.com>
2019-05-30 22:08:55 -0700

    Tower docs

--- src/entity/towers/tower.rs
+++ src/entity/towers/tower.rs
@@ -15,11 +15,15 @@ use crate::game::consts::{
     BULLET_HEIGHT,
 };
 
-pub enum TowerState{
+/// Enumeration describing the states of the Tower. While Ready, if an Enemy is
+/// in range, the Tower will start shooting and change it's state to Attacking.
+/// While Attacking, the Tower won't be able to fire any more bullets.
+pub enum TowerState {
     Ready,
     Attacking,
 }
 
+/// A struct describing the towers in the game. 
 pub struct Tower {
     pub base_shape: GenericShape,
     pub cannon_shape: GenericShape,
@@ -29,6 +33,8 @@ pub struct Tower {
 }
 
 impl Tower {
+    
+    /// Creates a new Tower. 
     pub fn new(position: Point2) -> Self {
         let base_shape = GenericShape::new(
             ShapeVariant::Circle{
@@ -64,12 +70,16 @@ impl Tower {
         }
     }
 
+    /// Sets the rotation of the Tower.
     pub fn set_rotation(&mut self, new_roation: f64) {
         self.cannon_shape.set_rotation(new_roation);
     }
 }
 
+
 impl Draw for Tower {
+    
+    /// Function to draw the Tower.
     fn draw<G: Graphics>(&self, c: &Context, g: &mut G){
         self.base_shape.draw(c, g);
         self.cannon_shape.draw(c, g);
@@ -85,6 +95,9 @@ impl Draw for Tower {
 impl State for Tower {
 
     type StateEnum = TowerState;
+
+    /// Function to change state. When setting the state to attacking, the 
+    /// bullet position is reset, and the rotation is set to match the Tower.
     fn change_state(&mut self, new_state: Self::StateEnum) {
         match new_state {
             TowerState::Attacking => {
@@ -109,6 +122,8 @@ impl State for Tower {
 }
 
 impl Entity for Tower {
+    
+    /// Function to move the Bullet each game Tick if Tower is Attacking.
     fn tick(&mut self) {
         match self.state {
             TowerState::Attacking => {
@@ -117,5 +132,6 @@ impl Entity for Tower {
             _ => (),
         }
     }
+    
 }
 
