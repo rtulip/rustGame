d16af94b1c45c39adb6769674871f2b1b99e1c3f
rtulip <robert.tulip97@gmail.com>
2019-06-01 17:29:58 -0700

    Added line_segment.rs

---
+++ src/math/line_segment.rs
@@ -0,0 +1,8 @@
+use crate::math::{Point2, Vec2};
+
+pub struct LineSegment {
+
+    pub point: Point2,
+    pub direction: Vec2,
+
+}
--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,6 +1,8 @@
 pub mod random;
 mod vector2d;
 mod point2d;
+mod line_segment;
 
 pub use self::vector2d::{Vec2};
 pub use self::point2d::{Point2};
+pub use self::line_segment::{LineSegment};
