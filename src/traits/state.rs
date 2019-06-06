b8bbf1f626ec6625e2978fbe54bf3c9d9df2bcfd
rtulip <robert.tulip97@gmail.com>
2019-06-06 12:17:44 -0700

    removed docs from traits which confused compiler

--- src/traits/state.rs
+++ src/traits/state.rs
@@ -1,13 +1,6 @@
 /// A trait to model state-based Entities. 
 /// Entity is a supertrait of State. (i.e. Implementing State requies Entity 
 /// to have been implemented)
-/// 
-/// type StateEnum: 
-///     The enum containing the different states
-/// 
-/// fn change_state()
-/// 
-///     Defines how to changes the state of the Entity to a new state.
 pub trait State {
     type StateEnum;
     fn change_state(&mut self, new_state: Self::StateEnum);
