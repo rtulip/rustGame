8f2ed416e08a732420df529fb588877eabf55c9d
rtulip <robert.tulip97@gmail.com>
2019-06-13 22:04:03 -0700

    Unit Testing Setup (#132)
    * Added basic tests for Point2
    
    * Vector2d unit tests
    
    * fixed point2 test_point_sub
    
    * updated level.rs docs examples
    
    * Updated map docs examples
    
    * removed docs from traits which confused compiler
    
    * Added basic unit testing for Level
    
    * Added integration tests folder
    
    * Added pathfinding tests - currently failing
    
    * Started adding testing
    
    * Added test_project
    
    * Added test for find extrema

--- src/math/collision.rs
+++ src/math/collision.rs
@@ -67,7 +67,7 @@ fn project(vec: Vec2, line: Vec2) -> Point2 {
     let norm = Vec2::new_unit(line.x, line.y);
     let c = Vec2::dot_product(vec, norm);
 
-    Point2{x: norm.x * -c, y: norm.y * -c}
+    Point2{x: norm.x * c, y: norm.y * c}
 
 }
 
@@ -85,8 +85,8 @@ fn find_extrema(points: Vec<Point2>) -> Vec<Point2>{
             let line_seg = Vec2::new_from_point(line_seg);
 
             if Vec2::dot_product(d,d) > Vec2::dot_product(line_seg, line_seg) {
-                extremes[0] = extremes[i];
-                extremes[1] = extremes[j];
+                extremes[0] = points[i];
+                extremes[1] = points[j];
             }
         }
     }
@@ -99,19 +99,19 @@ fn find_extrema(points: Vec<Point2>) -> Vec<Point2>{
 /// of four corners.
 pub fn circle_rect_intersect(c: Point2, r: f64, corners: Vec<Point2>) -> bool {
 
-    let p1 = corners[0] + project(Vec2::new_from_point(corners[0] - c), Vec2::new(corners[1].x - corners[0].x, corners[1].y - corners[0].y));
+    let p1 = corners[0] - project(Vec2::new_from_point(corners[0] - c), Vec2::new(corners[1].x - corners[0].x, corners[1].y - corners[0].y));
     let d1 = Vec2::new_from_point(p1 - c);
     let d1 = Vec2::dot_product(d1, d1);
 
-    let p2 = corners[0] + project(Vec2::new_from_point(corners[0] - c), Vec2::new(corners[2].x - corners[0].x, corners[2].y - corners[0].y));     
+    let p2 = corners[0] - project(Vec2::new_from_point(corners[0] - c), Vec2::new(corners[2].x - corners[0].x, corners[2].y - corners[0].y));     
     let d2 = Vec2::new_from_point(p2 - c);
     let d2 = Vec2::dot_product(d2, d2);
 
-    let p3 = corners[2] + project(Vec2::new_from_point(corners[0] - c), Vec2::new(corners[3].x - corners[2].x, corners[3].y - corners[2].y));
+    let p3 = corners[2] - project(Vec2::new_from_point(corners[0] - c), Vec2::new(corners[3].x - corners[2].x, corners[3].y - corners[2].y));
     let d3 = Vec2::new_from_point(p3- c);
     let d3 = Vec2::dot_product(d3, d3);
 
-    let p4 = corners[1] + project(Vec2::new_from_point(corners[0] - c), Vec2::new(corners[3].x - corners[1].x, corners[3].y - corners[1].y));
+    let p4 = corners[1] - project(Vec2::new_from_point(corners[0] - c), Vec2::new(corners[3].x - corners[1].x, corners[3].y - corners[1].y));
     let d4 = Vec2::new_from_point(p4 - c);
     let d4 = Vec2::dot_product(d4, d4);
 
@@ -137,8 +137,8 @@ pub fn rect_rect_intersect(corners1: Vec<Point2>, corners2: Vec<Point2>) -> bool
     let norm = line.normal_unit();
     for point in corners2.iter() {
         let v = Vec2::new_from_point(corners1[0] - *point);
-        let py = corners1[0] + project(v, line);
-        let px = corners1[0] + project(v, norm);
+        let py = corners1[0] - project(v, line);
+        let px = corners1[0] - project(v, norm);
         proj_y1.push(px);
         proj_x1.push(py);
     }
@@ -147,8 +147,8 @@ pub fn rect_rect_intersect(corners1: Vec<Point2>, corners2: Vec<Point2>) -> bool
     let norm = line.normal_unit();
     for point in corners1.iter() {
         let v = Vec2::new_from_point(corners2[0] - *point);
-        let py = corners2[0] + project(v, line);
-        let px = corners2[0] + project(v, norm);
+        let py = corners2[0] - project(v, line);
+        let px = corners2[0] - project(v, norm);
         proj_y2.push(px);
         proj_x2.push(py);
     }
@@ -173,5 +173,125 @@ pub fn circle_circle_intersect(c1: Point2, r1: f64, c2: Point2, r2: f64) -> bool
     let d = Vec2::dot_product(d, d);
 
     d < (r1 + r2).powi(2)
+
+}
+
+#[cfg(test)]
+mod collision_tests {
+
+    #[test]
+    fn test_within() {
+        use super::within;
+
+        assert!(!within(-1.0, 0.0, 2.0));
+        assert!(within(0.0, 0.0, 2.0));
+        assert!(within(1.0, 0.0, 2.0));
+        assert!(within(2.0, 0.0, 2.0));
+        assert!(!within(3.0, 0.0, 2.0));
+
+    }
+
+    #[test]
+    fn test_point_on_line() {
+        use super::point_on_line;
+        use crate::math::Point2;
+
+        let l1 = Point2 {
+            x: 0.0,
+            y: 0.0,
+        };
+
+        let l2 = Point2 {
+            x: 4.0,
+            y: 2.0,
+        };
+
+        assert!(!point_on_line(Point2{x: -2.0, y: -1.0}, l1, l2));
+        assert!(point_on_line(Point2{x: 0.0, y: 0.0}, l1, l2));
+        assert!(point_on_line(Point2{x: 2.0, y: 1.0}, l1, l2));
+        assert!(point_on_line(Point2{x: 4.0, y: 2.0}, l1, l2));
+        assert!(!point_on_line(Point2{x: 6.0, y: 3.0}, l1, l2));
+        
+        let l1 = Point2 {
+            x: 0.0,
+            y: 0.0,
+        };
+
+        let l2 = Point2 {
+            x: 0.0,
+            y: 2.0,
+        };
+
+        assert!(!point_on_line(Point2{x: 0.0, y: -1.0}, l1, l2));
+        assert!(point_on_line(Point2{x: 0.0, y: 0.0}, l1, l2));
+        assert!(point_on_line(Point2{x: 0.0, y: 1.0}, l1, l2));
+        assert!(point_on_line(Point2{x: 0.0, y: 2.0}, l1, l2));
+        assert!(!point_on_line(Point2{x: 0.0, y: 3.0}, l1, l2));
+        
+    }
+
+    #[test]
+    fn test_line_intersection() {
+
+        use super::line_intersection;
+        use crate::math::Point2; 
+
+        let line = [Point2{x: 0.0, y: 0.0}, Point2{x: 4.0, y: 2.0}];
+
+        let l = [Point2{x: -6.0, y: -3.0}, Point2{x: -2.0, y: -1.0}];
+        assert!(!line_intersection(l[0], l[1], line[0], line[1]));
+
+        let l = [Point2{x: -4.0, y: -2.0}, Point2{x: 0.0, y: 0.0}];
+        assert!(line_intersection(l[0], l[1], line[0], line[1]));
+
+        let l = [Point2{x: -2.0, y: 1.0}, Point2{x: 2.0, y: 1.0}];
+        assert!(line_intersection(l[0], l[1], line[0], line[1]));
+
+        let l = [Point2{x: 0.0, y: 0.0}, Point2{x: 4.0, y: 2.0}];
+        assert!(line_intersection(l[0], l[1], line[0], line[1]));
+
+        let l = [Point2{x: 2.0, y: 1.0}, Point2{x: 6.0, y: 3.0}];
+        assert!(line_intersection(l[0], l[1], line[0], line[1]));
+
+        let l = [Point2{x: 4.0, y: 2.0}, Point2{x: 8.0, y: 4.0}];
+        assert!(line_intersection(l[0], l[1], line[0], line[1]));
+
+        let l = [Point2{x: 6.0, y: 3.0}, Point2{x: 10.0, y: 5.0}];
+        assert!(!line_intersection(l[0], l[1], line[0], line[1]));
+
+    }
+
+    #[test]
+    fn test_project() {
+
+        use super::project;
+        use crate::math::Vec2;
+
+        let line = Vec2::new(4.0, 0.0);
+        
+        let vec = Vec2::new(1.0, -5.0);
+        let p = project(vec, line);
+        println!("p: {:?}", p);
+        assert!(p.x == 1.0 && p.y == 0.0);
+
+    }
+
+    #[test]
+    fn test_find_extrema() {
+
+        use super::find_extrema;
+        use crate::math::Point2;
+
+        let points = vec![
+            Point2{x: -2.0, y: -1.0},
+            Point2{x: 0.0, y: 0.0},
+            Point2{x: 2.0, y: 1.0},
+            Point2{x: 4.0, y: 2.0}
+        ];
+
+        let extremes = find_extrema(points);
+        assert!(extremes[0].x == -2.0 && extremes[0].y == -1.0);
+        assert!(extremes[1].x == 4.0 && extremes[1].y == 2.0);
+    }
 
 }
